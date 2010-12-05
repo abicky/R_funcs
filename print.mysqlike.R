ff02722f6c2ce33fa7a23a3a619fcc2344848468
abicky <takeshi.arabiki@gmail.com>
2010-12-05 13:51:15 +0900

    first commit

---
+++ print.mysqlike.R
@@ -0,0 +1,169 @@
+#-----------------------------------------------------------------------------------
+# print.mysqlike.R
+#
+# The MIT License
+#
+# Copyright (c) 2010 Takeshi Arabiki
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-----------------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------------
+# Description:
+#    output a vector, factor, matrix, list, or data.frame like MySQL
+#
+# Usage:
+#    print.mysqlike(texts, index, silent, header)
+#
+# Arguments:
+#    texts:      a vector, factor, matrix, list, or data.frame.
+#    index:      logical, whether to add row numbers.
+#    silent:     logical, whether to output the mysqlike class object.
+#    header:     logical, whether to add the table header.
+#-----------------------------------------------------------------------------------
+print.mysqlike <- function(texts, index = FALSE, silent = FALSE, header = TRUE) {
+    sep <- " | "; leftside = "| "; rightside = " |"
+    if (class(texts) != "mysqlike" || is.null(attr(texts, "formed"))) {
+        texts <- convertMatrix(texts, index)
+        # calculate max width for each column
+        width <- apply(rbind(colnames(texts), texts), 2, function(strs) max(nchar(strs, type = "width")))
+        border <- sapply(width + 2, function(n) paste(rep("-", n), collapse = ""))
+        border <- paste("+", paste(border, collapse = "+"), "+", sep = "")
+
+        records <- apply(texts, 1, concat, width = width, sep = sep, leftside = leftside, rightside = rightside)
+        
+        if (header) {            
+            header <- concat(colnames(texts), width = width, sep = sep, leftside = leftside, rightside = rightside)
+            header <- paste(border, header, sep = "\n")
+            out <- paste(header, border, paste(records, collapse = "\n"), border, "", sep = "\n")
+        } else {
+            out <- paste(border, paste(records, collapse = "\n"), border, "", sep = "\n")
+        }
+        
+        class(out) <- "mysqlike"
+        attr(out, "formed") <- TRUE
+    } else {
+        out <- texts
+    }
+    
+    if (!silent) {
+        cat(out)
+    }
+    
+    invisible(out)
+}
+
+
+
+#-----------------------------------------------------------------------------------
+# Description:
+#    convert a vector, factor, list, data.frame to a matrix
+#
+# Usage:
+#    convertMatrix(texts, index = FALSE)
+#
+# Arguments:
+#    texts:      a vector, factor, matrix, list, or data.frame.
+#    index:      logical, whether to add row numbers.
+#-----------------------------------------------------------------------------------
+convertMatrix <- function(texts, index = FALSE) {
+    if (mode(texts) == "list") {
+        len <- sapply(texts, length)
+        # unify the vector lengths of each list element, and texts become a matrix
+        texts <- mapply(function(x, n) c(as.character(x), rep("", n)), texts, max(len) - len)
+    } else if (is.vector(texts) || is.factor(texts)) {
+        texts <- as.matrix(texts)
+    } else {
+        if (!is.matrix(texts)) {
+            stop(sprintf("Invalid argument: '%s'!", as.character(substitute(texts))))
+        }
+    }
+    # if texts doesn't have the column name, add the column index
+    if (is.null(colnames(texts))) {
+        colnames(texts) <- 1:ncol(texts)
+    }
+    # if index is TRUE, add the row number
+    if (index) {
+        texts <- cbind(1:nrow(texts), texts)
+    }
+    texts
+}
+
+
+
+#-----------------------------------------------------------------------------------
+# Description:
+#    concatenate R objects as characters
+#
+# Usage:
+#    concat(..., width = NULL, sep = "", side = sep, leftside, rightside, left)
+#
+# Arguments:
+#    ...:        R objects. each object is converted to a character.
+#    width:      a numeric vector to specify widths of each element
+#    sep:        a character string to separate the terms.
+#    side:       a character add to either side. The value of sep is used by default.
+#                If leftside or rightside are specified, this value is ignored.
+#    leftside:   a character add to left side. The value of side is used by default.
+#    rightside:  a character add to right side. The value of leftside is used by default.
+#    left:       a logical vector, whether to left-align.
+#                The elements regarded as numeric are right-aligned
+#                and the others are left-aligned by default.
+#-----------------------------------------------------------------------------------
+concat <- function(..., width = NULL, sep = "", side = sep, leftside, rightside, left) {
+    strs <- unlist(list(...))
+    if (missing(leftside)) {
+        leftside = side
+    }
+    if (missing(rightside)) {
+        rightside = leftside
+    }
+    if (missing(left)) {
+        left <- !grepl("^-?\\d[\\d,]*\\.?\\d*$", strs, perl = TRUE)
+    }
+    if (!is.null(width)) {
+        strs <- pad(strs, width, left)
+    }
+    paste(leftside, paste(strs, collapse = sep), rightside, sep = "")
+}
+
+
+
+#-----------------------------------------------------------------------------------
+# Description:
+#    fill spaces to unify the lengths of characters
+#
+# Usage:
+#    pad(strs, width, left = TRUE)
+#
+# Arguments:
+#    strs:       a character vector.
+#    width:      a vector to specify widths of each element.
+#    left:       a logical vector, whether to left-align.
+#-----------------------------------------------------------------------------------
+pad <- function(strs, width, left = TRUE) {    
+    left <- rep(left, len = length(strs))
+    ifelse(left,
+           # insert spaces into right side
+           paste(strs, sprintf("%*s", width - nchar(strs, type = "width"), ""), sep = ""),
+           # insert spaces into left side
+           paste(sprintf("%*s", width - nchar(strs, type = "width"), ""), strs, sep = ""))
+}
