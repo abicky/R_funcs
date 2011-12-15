9789c9a1ed85ecc900fbe5899d683527e95a18c8
abicky <takeshi.arabiki@gmail.com>
2011-12-16 01:56:40 +0900

    repmat.R * use kronecker function instead of abind function

--- repmat.R
+++ repmat.R
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------------#
-# Copyright (c) 2010 Takeshi Arabiki                                                  #
+# Copyright (c) 2010-2011 Takeshi Arabiki                                             #
 # Licensed under the terms of the MIT License (see LICENSE.txt)                       #
 #-------------------------------------------------------------------------------------#
 
@@ -18,49 +18,33 @@
 #            to specify the number of replicatoin about each dimension.               #
 #    n:      an integer ignored if the number of dimensions of m is larger than 1.    #
 #-------------------------------------------------------------------------------------#
-library(abind)
 repmat <- function(A, m, n = m) {
     if (missing(m))
       stop("Requires at least 2 inputs.")
-    
-    if (is.vector(A))
-      A <- t(matrix(A))
-    
-    m[2] <- ifelse(is.na(m[2]), n, m[2])
-    if (!is.int(m) || any(m <= 0))
-      stop("'m' should be a nonnegative integer or a nonnegative integer vector!")
-    
-    d <- dim(A)
-    nr <- d[1L]
-    nc <- d[2L]
-    
-    if (length(d) <= 2) {
-        if (length(m) == 2) {
-            # about the same as array(A, m) even if length(A) == 1
-            # fastest!
-            tmpA <- matrix(t(A), nrow = nr * m[1], ncol = nc, byrow = TRUE)
-            matrix(tmpA, nrow = nr * m[1], ncol = nc * m[2])
-            # slower if number of rows is large?
-            # matrix(apply(A, 1, rep, m[1]), nrow = nr * m[1], ncol = nc * m[2], byrow = T)
-            # slower if number of columns is large?
-            # matrix(apply(A, 2, rep, m[2]), nrow = nr * m[1], ncol = nc * m[2])
-        } else {
-            tmpA <- matrix(t(A), nrow = nr * m[1], ncol = nc, byrow = TRUE)
-            tmpA <- matrix(tmpA, nrow = nr * m[1], ncol = nc * m[2])
-            array(tmpA, c(nr * m[1], nc * m[2], m[-(1:2)]))
-        }
-    } else {
-        for (i in 1:length(m)) {
-            A <- abind(rep(list(A), m[i]), along = i)
-        }
-        A
-    }
-}
 
-is.int <- function(n) {
-    if (!is.numeric(n)) {
-        FALSE
-    } else {
-        all(n %% 1 == 0)
+    if (is.vector(A))
+      A <- matrix(A, 1)
+
+    if (length(m) == 1)
+      m[2] <- n
+
+    if (any(as.integer(m) != m) || any(m <= 0)) {
+        stop("'m' should be a nonnegative integer or a nonnegative integer vector!")
     }
+
+    d <- dim(A)
+    if (length(d) <= 2) {
+        nr <- d[1L]
+        nc <- d[2L]
+        # kronecker(array(1, m), A) is slower
+        tmpA <- matrix(t(A), nrow = nr * m[1], ncol = nc, byrow = TRUE)
+        A <- matrix(tmpA, nrow = nr * m[1], ncol = nc * m[2])
+        if (length(m) > 2) {
+            A <- array(tmpA, c(nr * m[1], nc * m[2], m[-(1:2)]))
+        }
+    } else {
+        A <- kronecker(array(1, m), A)
+    }
+
+    A
 }
